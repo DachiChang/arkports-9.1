879bb1a9a3e5ec7058b0eff31037452c1259b6a1
Dachi Chang <femc7488@gmail.com>
2012-11-24 20:13:18 +0800

    patch user xorg/xclient-setting file

--- openbox-conf/files/install.sh
+++ openbox-conf/files/install.sh
@@ -2,6 +2,22 @@
 #
 # install openbox configure for user
 #
+update_config_file() {
+    if [ ! -f $1 ]; then
+        echo "# Begin of $3 settings" >> $1
+        cat $2 >> $1
+        echo "# End of $3 settings" >> $1
+    else
+        if ! grep "$3" $1 > /dev/null 2>&1; then
+            echo "# Begin of $3 settings" >> $1
+            cat $2 >> $1
+            echo "# End of $3 settings" >> $1
+        fi
+    fi
+    chown -f $USER_NAME $1
+    chmod 644 $1
+}
+
 if [ -z $1 ]; then
     USER_NAME=$USER
 else
@@ -17,3 +33,5 @@ install -o $USER_NAME -m 644 /usr/local/share/examples/openbox-conf/menu.xml $US
 install -o $USER_NAME -m 644 /usr/local/share/examples/openbox-conf/startup.wav $USER_HOME/$CONFIG_PATH
 install -o $USER_NAME -m 755 /usr/local/share/examples/openbox-conf/autostart $USER_HOME/$CONFIG_PATH
 install -o $USER_NAME -m 644 /usr/local/share/examples/openbox-conf/xinitrc $USER_HOME/.xinitrc
+install -d -o $USER_NAME -m 755 $USER_HOME/.config/xorg
+update_config_file $USER_HOME/.config/xorg/xclient-setting /usr/local/share/examples/openbox-conf/xclient-setting 'openbox-conf'
