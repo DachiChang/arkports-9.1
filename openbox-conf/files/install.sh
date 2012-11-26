fdc7dd46cd3002aa6e2a357e7eca751fc9fca333
Dachi Chang <femc7488@gmail.com>
2012-11-26 22:52:00 +0800

    install language-setting script

--- openbox-conf/files/install.sh
+++ openbox-conf/files/install.sh
@@ -2,22 +2,6 @@
 #
 # install openbox configure for user
 #
-update_config_file() {
-    if [ ! -f $1 ]; then
-        echo "# Begin of $3 settings" >> $1
-        cat $2 >> $1
-        echo "# End of $3 settings" >> $1
-    else
-        if ! grep "$3" $1 > /dev/null 2>&1; then
-            echo "# Begin of $3 settings" >> $1
-            cat $2 >> $1
-            echo "# End of $3 settings" >> $1
-        fi
-    fi
-    chown -f $USER_NAME $1
-    chmod 644 $1
-}
-
 if [ -z $1 ]; then
     USER_NAME=$USER
 else
@@ -32,6 +16,8 @@ install -o $USER_NAME -m 644 /usr/local/share/examples/openbox-conf/rc.xml $USER
 install -o $USER_NAME -m 644 /usr/local/share/examples/openbox-conf/menu.xml $USER_HOME/$CONFIG_PATH
 install -o $USER_NAME -m 644 /usr/local/share/examples/openbox-conf/startup.wav $USER_HOME/$CONFIG_PATH
 install -o $USER_NAME -m 755 /usr/local/share/examples/openbox-conf/autostart $USER_HOME/$CONFIG_PATH
-install -o $USER_NAME -m 644 /usr/local/share/examples/openbox-conf/xinitrc $USER_HOME/.xinitrc
+XORG_CONFIG_PATH='.config/xorg/xclient.d/'
 install -d -o $USER_NAME -m 755 $USER_HOME/.config/xorg
-update_config_file $USER_HOME/.config/xorg/xclient-setting /usr/local/share/examples/openbox-conf/locale_setting 'openbox-conf'
+install -d -o $USER_NAME -m 755 $USER_HOME/.config/xorg/xclient.d
+install -o $USER_NAME -m 755 /usr/local/share/examples/openbox-conf/language-setting $USER_HOME/$XORG_CONFIG_PATH
+install -o $USER_NAME -m 644 /usr/local/share/examples/openbox-conf/xinitrc $USER_HOME/.xinitrc
