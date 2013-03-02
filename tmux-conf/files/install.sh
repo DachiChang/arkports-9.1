1deb291b6273392346a656d9e5c724aa4506f569
Dachi Chang <femc7488@gmail.com>
2013-03-02 14:41:15 +0800

    new ports terminal-multiplexer

---
+++ tmux-conf/files/install.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+#
+# install tmux configure for user
+#
+if [ -z $1 ]; then
+    USER_NAME=$USER
+else
+    USER_NAME=$1
+fi
+USER_HOME=`grep "^$USER_NAME:" /etc/passwd | cut -d : -f 6`
+
+install -o $USER_NAME -m 644 /usr/local/share/examples/tmux-conf/tmux.conf $USER_HOME/.tmux.conf
