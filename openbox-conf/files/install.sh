a8166aa7061361514e03959954b815174a61d3a2
Dachi Chang <femc7488@gmail.com>
2013-03-21 00:33:36 +0800

    revert menu setting

--- openbox-conf/files/install.sh
+++ openbox-conf/files/install.sh
@@ -21,4 +21,5 @@ XORG_CONFIG_PATH='.config/xorg/xclient.d/'
 install -d -o $USER_NAME -m 755 $USER_HOME/.config/xorg
 install -d -o $USER_NAME -m 755 $USER_HOME/.config/xorg/xclient.d
 install -o $USER_NAME -m 755 /usr/local/share/examples/openbox-conf/language-setting $USER_HOME/$XORG_CONFIG_PATH
+install -o $USER_NAME -m 755 /usr/local/share/examples/openbox-conf/menu-setting $USER_HOME/$XORG_CONFIG_PATH
 install -o $USER_NAME -m 644 /usr/local/share/examples/openbox-conf/xinitrc $USER_HOME/.xinitrc
